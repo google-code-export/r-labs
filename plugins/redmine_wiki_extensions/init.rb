117e747a5a31e3584d0c22ce3216b520c224bb6a
haru.ii.a9i1e <haru.ii.a9i1e@8232fc7e-3114-11de-9d05-31133e6853b1>
2009-06-08 13:01:14 +0000

    fixes #78. 他のプロジェクトのwikiページにリンクを張るマクロを追加。

--- plugins/redmine_wiki_extensions/init.rb
+++ plugins/redmine_wiki_extensions/init.rb
@@ -19,6 +19,7 @@ require 'wiki_extensions_application_hooks'
 require 'wiki_extensions_wiki_page_patch'
 require 'wiki_extensions_footnote'
 require 'wiki_extensions_comments'
+require 'wiki_extensions_wiki_macro'
 
 Redmine::Plugin.register :redmine_wiki_extensions do
   name 'Redmine Wiki Extensions plugin'
