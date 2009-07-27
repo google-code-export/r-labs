b22d693573eb22a6f24852ea53b7f1fcd95ba8d5
haru.ii.a9i1e <haru.ii.a9i1e@8232fc7e-3114-11de-9d05-31133e6853b1>
2009-07-27 14:17:30 +0000

    Code Review Plugin 0.2.0 Released.

---
+++ tags/redmine_code_review-0.2.0/app/controllers/code_review_settings_controller.rb
@@ -0,0 +1,53 @@
+class CodeReviewSettingsController < ApplicationController
+  unloadable
+  layout 'base'
+  menu_item :code_review
+
+  before_filter :find_project, :authorize, :find_user
+
+  def show
+    @setting = CodeReviewProjectSetting.find(:first, :conditions => ['project_id = ?', @project.id])
+    @setting = CodeReviewProjectSetting.new unless @setting
+
+    @old_reviews = find_old_reviews
+  end
+
+
+  def update   
+    @setting = CodeReviewProjectSetting.find(:first, :conditions => ['project_id = ?', @project.id])
+    unless @setting
+      @setting = CodeReviewProjectSetting.new
+      @setting.project_id = @project.id
+    end
+
+    @setting.attributes = params[:setting]
+    @setting.updated_by = @user_id
+
+    @setting.save!
+    convert = params[:convert] unless params[:convert].blank?
+    if (convert and convert == 'true')
+      old_reviews = find_old_reviews
+      old_reviews.each {|review|
+        review.convert_to_new_data
+        review.destroy
+      }
+    end
+    flash[:notice] = l(:notice_successful_update)
+    redirect_to :action => "show", :id => @project
+  end
+
+  private
+  def find_project
+    # @project variable must be set before calling the authorize filter
+    @project = Project.find(params[:id])
+  end
+
+  def find_user
+    @user = User.current
+  end
+
+  def find_old_reviews
+    CodeReview.find(:all,
+      :conditions => ['issue_id is ? and old_parent_id is ? and project_id = ?', nil, nil, @project.id])
+  end
+end
